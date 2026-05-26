7d817f2bf14c4c9f9a542afc46b2e7ead5af242f
biafraga <beatrizcfraga3@gmail.com>
2026-05-26 11:46:00 -0300

    feat: continuação do método Get

--- src/main/java/br/com/vollmed/Api/controller/MedicoController.java
+++ src/main/java/br/com/vollmed/Api/controller/MedicoController.java
@@ -31,20 +31,23 @@ public class MedicoController {
         return medicoRepository.findAll();
     }
 
-    //@GetMapping("listar") // SPRING WEB -Informa que o método é do tipo GET(LEITURA)
-    //public List<DadosListagemMedico> listarRegraNegocio(){
-        //return medicoRepository.findAll();
+    @GetMapping("listar") // SPRING WEB -Informa que o método é do tipo GET(LEITURA)
+    public List<DadosListagemMedico> listarRegraNegocio(){
+        return medicoRepository.findAll().stream().map(DadosListagemMedico::new).toList();
+
         // findAll() -> Método que retorna uma lista de objetos do tipo DadosListagemMedico. 
         // stream() -> Método utilizado para transformar uma lista em um fluxo de dados, permitindo aplicar operações de transformação.
         // map() -> Método utilizado para converter cada objeto do tipo medico em um json DadosListagemMedico, utilizando o construtor que criamos em DadosListagemMedico. 
         // toList() -> Método utilizado para coletar os resultados (do map) em uma nova lista do tipo DadosListagemMedico, que é o formato que queremos retornar para a nossa API.
         // DadosListagemMedico::new -> É o construtor compacto (metódo de referência) que está no record DadosListagemMedico. 
-    //}
+    }
 
-    @PutMapping()
+    @PutMapping("atualizar")
     @Transactional
     public void atualizar(){
-
+        //Pegar o id do médico e guardar
+        //Verificar os dados que podem ser atualizados
+        //Verificar quais dados estão sendo atualizados
     }
 
 }
--- src/main/java/br/com/vollmed/Api/model/medico/DadosListagemMedico.java
+++ src/main/java/br/com/vollmed/Api/model/medico/DadosListagemMedico.java
@@ -7,5 +7,11 @@ public record DadosListagemMedico(
     String crm,
     Especialidade especialidade
 ) {
+
+    //Método construtor recebendo o objeto Medico e convertendo para DadosListagemMedico
+    //Forma recente.
+    public DadosListagemMedico(Medico medico){
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+    }
     
 }
