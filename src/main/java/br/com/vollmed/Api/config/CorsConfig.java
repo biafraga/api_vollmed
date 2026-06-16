7f0c1f573623ede75d0b60d8c8988d335becd68d
biafraga <beatrizcfraga3@gmail.com>
2026-06-16 16:09:51 -0300

    atualizações do dia 16/06

--- src/main/java/br/com/vollmed/Api/config/CorsConfig.java
+++ src/main/java/br/com/vollmed/Api/config/CorsConfig.java
@@ -10,8 +10,8 @@ public class CorsConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry
-        .addMapping("/**")
-        .allowedOrigins("*")
+        .addMapping("/**") // Aplicar em todas as rotas
+        .allowedOrigins("*") // qualquer origem
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .maxAge(3600);
--- src/main/java/br/com/vollmed/Api/security/JwtUtil.java
+++ src/main/java/br/com/vollmed/Api/security/JwtUtil.java
@@ -1,5 +1,14 @@
 package br.com.vollmed.Api.security;
 
 public class JwtUtil {
+    // Criar o token com dados do usuario
+    // Assinar com a nossa chave secreta
+    // Validar os dados recebidos
+
+    // Um token JWT tem 3 partes por ponto:
+    // HEADER.PAYLOAD.SIGNATURE
+   // 1. HEADER -> Tipo do token e o algoritmo da requisição.
+   // 2. PAYLOAD -> dados (claims) - aqui guardamos os dados do usuário, como nome e ROLE.
+   // 3. SIGNATURE -> assinatura gerada com a chave secreta.
     
 }
